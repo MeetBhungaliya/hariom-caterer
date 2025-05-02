5c7704876cc8848e76eb39b48dda7fb9afe5206c
Meet Bhungaliya <meet@Meets-MacBook-Air.local>
2025-05-02 23:22:24 +0530

    updated app component path

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
+import App from '@/app'
 import { Toaster } from './components/ui/sonner'
 
 const rootElement = document.getElementById('root')
