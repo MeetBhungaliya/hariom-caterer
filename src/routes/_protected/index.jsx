6d4d2d6c098ca00a4bd81ae74fde5966a869e733
Meet Bhungaliya <meet@Meets-MacBook-Air.local>
2025-06-04 21:35:10 +0530

    updated github action

--- src/routes/_protected/index.jsx
+++ src/routes/_protected/index.jsx
@@ -1,4 +1,5 @@
 import { getDashboard } from "@/api/query-option";
+import NoData from "@/components/common/NoData";
 import { Table } from "@/components/common/table";
 import { useAuthStore } from "@/hooks/use-auth";
 import { useQuery } from "@tanstack/react-query";
@@ -12,76 +13,78 @@ export const Route = createFileRoute("/_protected/")({
 });
 
 function RouteComponent() {
-  const isLoading = useAuthStore(state => state.isLoading)
+  const isLoading = useAuthStore((state) => state.isLoading);
   const dashboardData = useQuery(getDashboard());
 
-   const columns = useMemo(() => [
-    {
-      header: 'Order Id',
-      accessorKey: 'order_id',
-      size: 200,
-    },
-    {
-      header: 'Client Name',
-      accessorKey: 'client.name',
-      size: 200,
-    },
-    {
-      header: 'Date & Time',
-      accessorKey: 'date',
-      cell: props => (
-        <span>
-          {moment(props.getValue()).format('DD-MM-YYYY')}
-          &nbsp;/&nbsp;
-          {props.row.original.time}
-        </span>
-      ),
-      size: 200,
-    },
-    {
-      header: 'Person',
-      accessorKey: 'person',
-      size: 200,
-    },
-    {
-      header: 'Venue',
-      accessorKey: 'venue',
-      size: 200,
-    },
-    {
-      header: 'Jain Counter',
-      accessorKey: 'jain_counter',
-      size: 200,
-    },
-    {
-      header: 'Per Plate Cost',
-      accessorKey: 'per_plate_cost',
-      size: 200,
-    },
-    {
-      header: 'Selling Price',
-      accessorKey: 'selling_price',
-      size: 200,
-    },
-    {
-      header: 'Pro',
-      accessorKey: 'pro',
-      size: 200,
-    },
-    {
-      header: 'Bom Boys',
-      accessorKey: 'bom_boys',
-      size: 200,
-    },
-    {
-      header: 'Packed Bottle',
-      accessorKey: 'packed_bottle',
-      size: 200,
-    },
-  ], [])
+  const columns = useMemo(
+    () => [
+      {
+        header: "Order Id",
+        accessorKey: "order_id",
+        size: 200,
+      },
+      {
+        header: "Client Name",
+        accessorKey: "client.name",
+        size: 200,
+      },
+      {
+        header: "Date & Time",
+        accessorKey: "date",
+        cell: (props) => (
+          <span>
+            {moment(props.getValue()).format("DD-MM-YYYY")}
+            &nbsp;/&nbsp;
+            {props.row.original.time}
+          </span>
+        ),
+        size: 200,
+      },
+      {
+        header: "Person",
+        accessorKey: "person",
+        size: 200,
+      },
+      {
+        header: "Venue",
+        accessorKey: "venue",
+        size: 200,
+      },
+      {
+        header: "Jain Counter",
+        accessorKey: "jain_counter",
+        size: 200,
+      },
+      {
+        header: "Per Plate Cost",
+        accessorKey: "per_plate_cost",
+        size: 200,
+      },
+      {
+        header: "Selling Price",
+        accessorKey: "selling_price",
+        size: 200,
+      },
+      {
+        header: "Pro",
+        accessorKey: "pro",
+        size: 200,
+      },
+      {
+        header: "Bom Boys",
+        accessorKey: "bom_boys",
+        size: 200,
+      },
+      {
+        header: "Packed Bottle",
+        accessorKey: "packed_bottle",
+        size: 200,
+      },
+    ],
+    []
+  );
 
-  if (dashboardData.isError)
-    return null
+  if (dashboardData.isError) return null;
 
   return (
     <div className="h-full flex flex-col gap-y-6">
@@ -117,12 +120,18 @@ function RouteComponent() {
           </div>
         </div>
       </div>
-      <Table
-        columns={columns}
-        data={dashboardData.data.result.order}
-        isLoading={isLoading || dashboardData.fetchStatus === "fetching"}
-        pagination={false}
-      />
+      {dashboardData.data.result.order?.length ||
+      isLoading ||
+      dashboardData.fetchStatus === "fetching" ? (
+        <Table
+          columns={columns}
+          data={dashboardData.data.result.order}
+          isLoading={isLoading || dashboardData.fetchStatus === "fetching"}
+          pagination={false}
+        />
+      ) : (
+        <NoData />
+      )}
     </div>
   );
 }
